8e77193ba09ab2f449be57f679b20eb2cab24687
thomas <thomas.sedlmair@googlemail.com>
2023-02-27 22:11:56 +0100

    added naiv ecs approach

--- src/game.hpp
+++ src/game.hpp
@@ -14,6 +14,102 @@
 
 namespace cwt {
 
+using entity = std::size_t;
+entity max_entity = 0;
+std::size_t create_entity()
+{
+    static std::size_t entities = 0;
+    ++entities;
+    max_entity = entities;
+    return entities;
+}
+
+struct sprite_component{
+    SDL_Rect src;
+    SDL_Rect dst;
+    SDL_Texture* texture;
+};
+
+struct transform_component{
+    float pos_x;
+    float pos_y;
+    float vel_x;
+    float vel_y;
+};
+
+struct keyinputs_component{ };
+
+
+struct registry {
+    std::unordered_map<entity, sprite_component> sprites;
+    std::unordered_map<entity, transform_component> transforms;
+    std::unordered_map<entity, keyinputs_component> keys;
+};
+
+struct sprite_system 
+{
+    void update(registry& reg)
+    {
+        for (int e = 1 ; e <= max_entity ; e++) {
+            if (reg.sprites.contains(e) && reg.transforms.contains(e)){
+                reg.sprites[e].dst.x = reg.transforms[e].pos_x;
+                reg.sprites[e].dst.y = reg.transforms[e].pos_y;
+            }
+        }
+    }
+    void render(registry& reg, SDL_Renderer* renderer)
+    {
+        for (int e = 1 ; e <= max_entity ; e++) {
+            if (reg.sprites.contains(e)){
+                SDL_RenderCopy(
+                    renderer, 
+                    reg.sprites[e].texture, 
+                    &reg.sprites[e].src, 
+                    &reg.sprites[e].dst
+                );
+            }
+        }
+    }
+};
+
+struct transform_system 
+{
+    float dt = 0.1f;
+    
+    void update(registry& reg)
+    {
+        for (int e = 1 ; e <= max_entity ; e++) {
+            if (reg.transforms.contains(e)){
+                reg.transforms[e].pos_x += reg.transforms[e].vel_x*dt;
+                reg.transforms[e].pos_y += reg.transforms[e].vel_y*dt;
+            }
+        }
+    }
+};
+
+struct movement_system 
+{  
+    void update(registry& reg)
+    {
+        const Uint8* keys = SDL_GetKeyboardState(NULL);
+
+        for (int e = 1 ; e <= max_entity ; e++) {
+            if (reg.transforms.contains(e) && reg.keys.contains(e)){
+                
+                if (keys[SDL_SCANCODE_A]) { reg.transforms[e].vel_x = -1.0f; } 
+                if (keys[SDL_SCANCODE_S]) { reg.transforms[e].vel_y = 1.0f; }
+                if (keys[SDL_SCANCODE_W]) { reg.transforms[e].vel_y = -1.0f; }
+                if (keys[SDL_SCANCODE_D]) { reg.transforms[e].vel_x = 1.0f; }
+                
+                if (!keys[SDL_SCANCODE_A] && !keys[SDL_SCANCODE_D]) { reg.transforms[e].vel_x = 0.0f; }
+                if (!keys[SDL_SCANCODE_S] && !keys[SDL_SCANCODE_W]) { reg.transforms[e].vel_y = 0.0f; }
+            } 
+        }
+    }
+    
+};
+
+
 class game
 {
     public: 
@@ -46,6 +142,9 @@ class game
             SDL_Quit();
         }
 
+        registry& get_registry() { return m_registry; }
+        SDL_Renderer* get_renderer() { return m_renderer; }
+
         bool is_running()
         {
             return m_is_running;
@@ -62,21 +161,32 @@ class game
         }
         void update()
         {
-
+            m_transform_system.update(m_registry);
+            m_movement_system.update(m_registry);
+            m_sprite_system.update(m_registry);
         }
         void render()
         {
             SDL_RenderClear(m_renderer);
 
+            m_sprite_system.render(m_registry, m_renderer);
+
             SDL_RenderPresent(m_renderer);
         }
-
+SDL_Renderer* m_renderer;
     private:
         std::size_t m_width;
         std::size_t m_height;
         SDL_Window* m_window; 
-        SDL_Renderer* m_renderer;
+        
         bool m_is_running;
+
+        registry m_registry;
+
+        sprite_system m_sprite_system;
+        transform_system m_transform_system;
+        movement_system m_movement_system;
+
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include "game.hpp"
-
+#include "bird.hpp"
 
 int main(int argc, char* argv[]) 
 {
     cwt::game game(800, 600);
 
+    cwt::entity bird = cwt::create_entity();
+    
+    game.get_registry().sprites[bird] = cwt::sprite_component {
+        SDL_Rect{0, 0, 300, 230}, 
+        SDL_Rect{10, 10, 100, 73}, 
+        IMG_LoadTexture(game.get_renderer(), bird_path)
+    };
+    game.get_registry().transforms[bird] = cwt::transform_component { 10, 10, 0, 0};
+    game.get_registry().keys[bird] = cwt::keyinputs_component { };
+    
     while(game.is_running()) 
     {
         game.read_input();
